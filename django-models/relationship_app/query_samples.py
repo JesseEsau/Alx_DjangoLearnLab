3e32000de21c8c8d48cb57a5842b18c96eee6df5
Esau Jesse <jesseesau13@gmail.com>
2024-08-12 12:21:09 +0100

    Update query_sample.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,10 +1,15 @@
-from relationship_app.models import Book, Library
+from relationship_app.models import Book, Library, Author
 
+# variables
 library_name = "ALX Modern Library"
-books_by_jesse = Book.objects.filter(author='Jesse Esau').values()
-books_by_jesse
+author_name = "Jesse Esau"
 
+# get all books by a specific author
+books_by_jesse = Author.objects.filter(author=author_name)
+
+# Retrieve all books from a given library
 books = Library.objects.get(name=library_name)
 books.all()
 
+# Retrieve librarian of a library
 librarian = Library.objects.get(id=1)
