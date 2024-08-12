37b2d1f922469f8240a2810b4c3c6652c79d2cbe
Esau Jesse <jesseesau13@gmail.com>
2024-08-12 12:13:24 +0100

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,10 +1,10 @@
 from relationship_app.models import Book, Library
 
+library_name = "ALX Modern Library"
 books_by_jesse = Book.objects.filter(author='Jesse Esau').values()
 books_by_jesse
 
-all_books = Library.objects.all()
-all_books
+books = Library.objects.get(name=library_name)
+books.all()
 
 librarian = Library.objects.get(id=1)
-librarian
