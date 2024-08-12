1ad9099651308fbe1e1fba1f48b469b2659941da
Esau Jesse <jesseesau13@gmail.com>
2024-08-12 12:50:34 +0100

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -6,7 +6,7 @@ author_name = "Jesse Esau"
 
 # get all books by a specific author
 author = Author.objects.get(name=author_name)
-books_by_jesse = Book.objects.filter(name=author)
+books_by_jesse = Book.objects.filter(author=author)
 
 # Retrieve all books from a given library
 books = Library.objects.get(name=library_name)
