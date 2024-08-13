931c9c491b6a07df5923728df4cb4fe9fe728a0c
Esau Jesse <jesseesau13@gmail.com>
2024-08-13 15:50:09 +0100

    Update views.py and urls.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import ListView
 
 # Create your views here.
+
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {
+        'book_list': books,
+    }
+    return render(request, "list_books.html", context)
+
+
+class LibraryDetailView(ListView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    queryset = Library.objects.all()
