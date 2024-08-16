906936708c5c75c1814ebfca4dab8aecab69c675
Esau Jesse <jesseesau13@gmail.com>
2024-08-16 22:29:28 +0100

    Added Role Based Access

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,13 +4,22 @@ from .views import LibraryDetailView
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
+
 urlpatterns = [
     path('books/', list_books),
     path('library/ <int:pk> /', LibraryDetailView.as_view(), name='library_detail'),
+
     path('', views.index, name="home"),
     path("register/", views.register, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html",
          next_page="home"), name="login"),
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html",
          next_page="home"), name="logout"),
+
+    path("admin/", admin_view, name="admin"),
+    path("librarian/", librarian_view, name="librarian"),
+    path("member/", member_view, name="member")
 ]
