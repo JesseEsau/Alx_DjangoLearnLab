5473f77b6e285c936a46d2932a9a74c9aeaba2bb
Esau Jesse <jesseesau13@gmail.com>
2024-08-17 12:56:16 +0100

    Update amin_view.py

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 
 
 def is_admin(user):
-    return user.userprofile.role == "Member"
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 
 @user_passes_test(is_admin, redirect_field_name="home")
 def admin_view(request):
+
     return render(request, 'relationship_app/admin_dashboard.html')
