389b41d4fdc936f6bef479fd388408e05650a360
Esau Jesse <jesseesau13@gmail.com>
2024-08-17 18:01:08 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,7 +3,9 @@ from .models import Book
 from .forms import BookForm
 from .models import Library
 
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
