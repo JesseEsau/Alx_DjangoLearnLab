a66093a99f808e396ab981df669f4f4377d6464e
Esau Jesse <jesseesau13@gmail.com>
2024-09-19 16:09:11 +0100

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
 ]
 
 # custom user
@@ -51,6 +52,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',  # Enable token authentication
     ],
 
+}
+
+# pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 MIDDLEWARE = [
