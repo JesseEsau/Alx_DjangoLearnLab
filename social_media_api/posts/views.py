92216c54b79145036623384a05c1ea874153949a
Esau Jesse <jesseesau13@gmail.com>
2024-09-21 12:36:03 +0100

    Add feed functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,12 +1,16 @@
 
 from rest_framework import viewsets, permissions
 from rest_framework import filters
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     queryset = Post.objects.all()
@@ -25,3 +29,16 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        following_users = user.following.all()
+        posts = Post.objects.filter(
+            author__in=following_users).order_by('-created_at')
+        serialized_posts = PostSerializer(posts, many=True)
+        return Response(serialized_posts.data)
