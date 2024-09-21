b9269fe669be882837acf4c9c5852fb396de3a36
Esau Jesse <jesseesau13@gmail.com>
2024-09-21 13:05:45 +0100

    Update posts/views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -38,7 +38,7 @@ class FeedView(APIView):
     def get(self, request):
         user = request.user
         following_users = user.following.all()
-        posts = Post.objects.filter(
-            author__in=following_users).order_by('-created_at')
+        Post.objects.filter(author__in=following_users).order_by
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serialized_posts = PostSerializer(posts, many=True)
         return Response(serialized_posts.data)
